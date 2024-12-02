4eaeb5e11d5c2db2ae984ecf5f25e96daaf1f366
Alanjosh457 <alanjoshua515@gmail.com>
2024-12-02 13:31:28 +0530

    Updated with hashrouter main.jsx

--- front7/src/main.jsx
+++ front7/src/main.jsx
@@ -4,12 +4,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import Header from './pages/Header.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <HashRouter>
       <Header />
-    </BrowserRouter>
+    <HashRouter/>
 
 );
